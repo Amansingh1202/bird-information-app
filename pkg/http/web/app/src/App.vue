0ac536897715b6e928f8b8bfc3cd4090c960bd3e
Amansingh1202 <aks001235@gmail.com>
2021-02-07 19:10:51 +0530

    Initial commit

---
+++ pkg/http/web/app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <DropdownDemo></DropdownDemo>
+</template>
+<script>
+import DropdownDemo from './components/DropdownDemo'
+export default {
+    components: {
+        DropdownDemo
+    }
+}
+</script>
+<style lang="scss">
+@import "./App.scss";
+</style>
